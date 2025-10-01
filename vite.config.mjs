400733f5c4db665b87a11392e8c9a96922646460
Juan421P <20210297@ricaldone.edu.sv>
2025-10-01 12:58:10 -0600

    commit inicial. Siguiendo la buena costumbre de que sea cabal un día antes de la defensa jeje

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	base: './',
-	build: {
-		outDir: 'dist',
-	},
 	plugins: [
 		tailwindcss(),
 	],
